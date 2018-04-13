9f766c2deff3c05f800b63ee3e6202728f32adba
astern <alena7690@gmail.com>
2018-04-13 18:37:03 -0500

    Final HW2 ready for grading

--- hws/explore.py
+++ hws/explore.py
@@ -4,23 +4,69 @@ import matplotlib.pyplot as plt
 import math
 import numpy as np
 
+def read_data(file_path):
+    '''
+    Function to read in data from csv into pandas dataframe
+    Input:
+        file_path (str): file path for location of csv file
+    Returns:
+        pandas dataframe
+        
+    '''
+    return pd.read_csv(file_path)
+
 def dist_table(data_frame):
+    '''
+    Function to create a table showing the distribution of each variable in a 
+    pandas data frame
+    Input:
+        data_frame (pandas dataframe)
+    Returns:
+        table showing the distribtion of each variable
+    '''
     return data_frame.describe()
 
 def dist_plot(data_frame):
+    '''
+    Function to plot a histogram of each variable to show the distribution
+    Input:
+        data_frame (pandas dataframe)
+    Returns:
+        grid of histogram plots for each variable in dataframe
+    '''
     plt.rcParams['figure.figsize'] = 16, 12
-    data.hist()
+    data_frame.hist()
     plt.show()
 
 def outliers_plot(data_frame):
+    '''
+    Produces box plot for all variables in the dataframe to inspect outliers
+    Input:
+        data_frame (pandas dataframe)
+    Returns:
+        grid of box plots for each variable in dataframe
+    '''
     plt.rcParams['figure.figsize'] = 16, 12
-    data_frame.plot(kind='box', subplots=True, layout=(4, math.ceil(len(data_frame.columns)/4)), sharex=False, sharey=False)
+    data_frame.plot(kind='box', subplots=True, 
+        layout=(4, math.ceil(len(data_frame.columns)/4)), 
+        sharex=False, sharey=False)
     plt.show()
 
 def corr_table(data_frame):
     return data_frame.corr(method = 'pearson')
 
 def plot_y_corr(corr_table, y):
+    '''
+    Produces a bar plot showing the correlation of each variable with the 
+    dependent variable
+
+    Inputs:
+        corr_table(pandas dataframe): dataframe of variable correlations produced
+        using corr_table function
+        y (str): name of dependent variable for analysis
+    Returns:
+        bar plot showing correlation of each variable with the dependent variable
+    '''
     corr_y = corr_table[y]
     title = "Correlation with {}".format(y)
     corr_y.plot.bar(title = title)
@@ -28,6 +74,19 @@ def plot_y_corr(corr_table, y):
     plt.show()
 
 def plot_corr_matrix(corr_table):
+    '''
+    Produces a plot showing the correlation between all sets of variables in
+    the dataframe
+
+    Input:
+        corr_table(pandas dataframe): dataframe of variable correlations produced
+        using corr_table function
+    returns:
+        plot showing correlations between all sets of variables in dataframe
+
+    reference: https://machinelearningmastery.com/visualize-machine-learning-data-python-pandas/
+
+    '''
     names = list(corr_table.index)
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -41,13 +100,22 @@ def plot_corr_matrix(corr_table):
     
     plt.show()
     
-    #https://machinelearningmastery.com/visualize-machine-learning-data-python-pandas/
-
+   
 def plot_by_class(data_frame, y):
+    '''
+    Produces plots for each variable in the dataframe showing distribution by
+    each value of the dependent variable
+
+    Inputs:
+        data_frame (pandas dataframe)
+        y (str): name of dependent variable for analysis
+    Returns:
+        layered histogram plots for each variable in the dataframe
+    '''
     plt.rcParams['figure.figsize'] = 5, 4
-    grp = data.groupby(y)
+    grp = data_frame.groupby(y)
     var = data_frame.columns
-    
+
     for v in var:
         getattr(grp, v).hist(alpha=0.4)
         plt.title(v)
